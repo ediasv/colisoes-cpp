505da43953119683b4133538c3cbcabde9f5736d
ediasv <eduvidias@gmail.com>
2024-09-03 10:19:53 -0300

    checkpoint

--- include/Ball.cpp
+++ include/Ball.cpp
@@ -9,8 +9,19 @@ void Ball::move() {
 
   // check for wall colision;
   // top
-  if (this->position.y >)
+  if (this->position.y - this->radius <= 0) {
+    this->velocity.y *= -1;
+  }
   // bottom
+  if (this->position.y + this->radius >= WIN_HEIGHT) {
+    this->velocity.y *= -1;
+  }
   // left
+  if (this->position.x - this->radius <= 0) {
+    this->velocity.x *= -1;
+  }
   // right
+  if (this->position.x + this->radius >= WIN_WIDTH) {
+    this->velocity.x *= -1;
+  }
 }
