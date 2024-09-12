50f5baaa945f5fc333f415834d64b60a55d1ff2b
ediasv <eduvidias@gmail.com>
2024-09-12 09:31:32 -0300

    checkpoint

--- include/Engine.hpp
+++ include/Engine.hpp
@@ -1,12 +1,26 @@
 #pragma once
 
-#include "Engine.hpp"
+#include "Geometry.hpp"
 #include <SFML/Graphics/CircleShape.hpp>
+#include <vector>
 
 namespace my {
 
 struct CircleRenderer {
-  sf::CircleShape a;
+  sf::CircleShape shape;
+
+  CircleRenderer(const geom::Circle &circle);
+
+  void updatePosition(const geom::Circle &circle);
+  sf::CircleShape getShape() const;
+};
+
+template <typename ShapeType> struct Shapes {
+  std::vector<ShapeType> shapes;
+
+  Shapes(int n_shapes);
+
+  void addShape(const ShapeType &shape);
 };
 
 } // namespace my
--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1,1 +1,24 @@
+#include "../include/Engine.hpp"
+#include <SFML/Graphics/CircleShape.hpp>
 
+using namespace my;
+
+CircleRenderer::CircleRenderer(const geom::Circle &circle) {
+  this->shape.setRadius(circle.radius);
+  this->shape.setPosition(circle.position.x, circle.position.y);
+}
+
+void CircleRenderer::updatePosition(const geom::Circle &circle) {
+  this->shape.setPosition(circle.position.x, circle.position.y);
+}
+
+sf::CircleShape CircleRenderer::getShape() const { return this->shape; }
+
+template <typename ShapeType> Shapes<ShapeType>::Shapes(int n_shapes) {
+  shapes.reserve(n_shapes);
+}
+
+template <typename ShapeType>
+void Shapes<ShapeType>::addShape(const ShapeType &shape) {
+  shapes.push_back(shape);
+}
